e1bc2179bfbd3dde06f1d904b96d6e125dedf906
Alan99661 <cholakovalan@gmail.com>
2023-06-07 22:59:19 +0300

    GameViewModels

---
+++ VideoGameApplication.Servises/ViewModels/GameViewModels/GameAddModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoGameApplication.Models.Entities;
+
+namespace VideoGameApplication.Servises.ViewModels.GameViewModels
+{
+    public class GameAddModel
+    {
+        public string Name { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string? Description { get; set; }
+        public List<string> DeveloperIds { get; set; }
+        public List<string> ReviewIds { get; set; }
+        public List<string> PlatformIds { get; set; }
+        public List<string> GenreIds { get; set; }
+        public List<string> ScreenshotIds { get; set; }
+        public string ScreenshotId { get; set; }
+        public int? MetacriticRating { get; set; }
+        public string GameWebsite { get; set; }
+    }
+}
---
+++ VideoGameApplication.Servises/ViewModels/GameViewModels/GameViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoGameApplication.Models.Entities;
+
+namespace VideoGameApplication.Servises.ViewModels.GameViewModels
+{
+    public class GameViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string? Description { get; set; }
+        public List<Developer> Developers { get; set; }
+        public List<Review> Reviews { get; set; }
+        public List<Platform> Platforms { get; set; }
+        public List<Genre> Genres { get; set; }
+        public List<Screenshot> Screenshots { get; set; }
+        public Screenshot Screenshot { get; set; }
+        public int? MetacriticRating { get; set; }
+        public string GameWebsite { get; set; }
+    }
+}
