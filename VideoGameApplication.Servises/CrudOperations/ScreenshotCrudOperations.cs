0b345225fa2a2b305437f132bb7f02c1da7a2f51
Alan99661 <cholakovalan@gmail.com>
2023-06-08 21:10:16 +0300

    ScreenshotCrudOperations

--- VideoGameApplication.Servises/CrudOperations/ScreenshotCrudOperations.cs
+++ VideoGameApplication.Servises/CrudOperations/ScreenshotCrudOperations.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VideoGameApplication.Database;
+using VideoGameApplication.Models.Entities;
+using VideoGameApplication.Servises.ViewModels.ScreenshotViewModels;
 
 namespace VideoGameApplication.Servises.CrudOperations
 {
@@ -17,6 +21,87 @@ namespace VideoGameApplication.Servises.CrudOperations
         {
             this.context = context;
             this.mapper = mapper;
+        }
+
+        public List<ScreenshotViewModel> GetAll()
+        {
+            try
+            {
+
+                var res = context.Screenshots
+                    .Select(s => s)
+                    .Include(s => s.Game)
+                    .ToList();
+                return mapper.Map<List<ScreenshotViewModel>>(res);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed");
+            }
+        }
+        public ScreenshotViewModel GetById(string id)
+        {
+            try
+            {
+
+
+                var res = context.Screenshots
+                    .Include(s => s.Game)
+                    .FirstOrDefault(s => s.Id == id);
+                return mapper.Map<ScreenshotViewModel>(res);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed");
+            }
+        }
+        public ScreenshotViewModel CreateScreenshot(ScreenshotAddModel addModel)
+        {
+            try
+            {
+
+                var screenshot = mapper.Map<Screenshot>(addModel);
+                var game = context.Games.FirstOrDefault(s => s.Id == addModel.GameId);
+                screenshot.Game = game;
+                context.Screenshots.Add(screenshot);
+                context.SaveChanges();
+                return mapper.Map<ScreenshotViewModel>(screenshot);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed");
+            }
+        }
+        public ScreenshotViewModel UpdateScreenshot(ScreenshotUpdateModel updateModel) 
+        {
+            try
+            {
+                var screenshot = context.Screenshots.FirstOrDefault(s => s.Id == updateModel.Id);
+                var game = context.Games.FirstOrDefault(s => s.Id == updateModel.GameId);
+                screenshot.Url = updateModel.Url;
+                screenshot.Game = game;
+                context.Update(screenshot);
+                context.SaveChanges();
+                return mapper.Map<ScreenshotViewModel>(screenshot);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Failed");
+            }
+        }
+        public string DeleteScreenshot(ScreenshotDeleteModel deleteModel)
+        {
+            try
+            {
+                var screenshot = context.Screenshots.FirstOrDefault(s => s.Id == deleteModel.Id);
+                context.Remove(screenshot);
+                context.SaveChanges();
+                return "Sucsess";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed");
+            }
         }
     }
 }
---
+++ VideoGameApplication.Servises/ViewModels/ScreenshotViewModels/ScreenshotUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoGameApplication.Servises.ViewModels.ScreenshotViewModels
+{
+    public class ScreenshotUpdateModel
+    { 
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public string GameId { get; set; }
+    }
+}
